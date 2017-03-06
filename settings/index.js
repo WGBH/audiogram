f693f5ef73a60f307c77f070207437eefd0e08c2
Tim Kinnel <tim_kinnel@wgbh.org>
2017-03-06 16:56:24 -0500

    Add potential world theme, including new font

--- settings/index.js
+++ settings/index.js
@@ -29,6 +29,10 @@ module.exports = {
     { family: "Source Sans Pro", file: path.join(__dirname, "fonts", "SourceSansPro-BoldItalic.ttf"), weight: "bold", style: "italic" },
     { family: "Burlingame Pro",  file: path.join(__dirname, "fonts", "BurlingamePro Light-AAPB.ttf"), weight: "300" },
     { family: "Lato Bold",       file: path.join(__dirname, "fonts", "Lato-Bold-Masterpiece.ttf") },
-    { family: "Trebuchet",       file: path.join(__dirname, "fonts", "Trebuchet MS.ttf") }
+    { family: "Trebuchet",       file: path.join(__dirname, "fonts", "Trebuchet MS.ttf") },
+    { family: "TeX Gyre Schola", file: path.join(__dirname, "fonts", "texgyreschola-regular.otf") },
+    { family: "TeX Gyre Schola", file: path.join(__dirname, "fonts", "texgyreschola-bold.otf"), weight: "bold" },
+    { family: "TeX Gyre Schola", file: path.join(__dirname, "fonts", "texgyreschola-italic.otf"), style: "italic" },
+    { family: "TeX Gyre Schola", file: path.join(__dirname, "fonts", "texgyreschola-bolditalic.otf"), weight: "bold", style: "italic" }
   ]
 };
